983b64081d2eaafa045072b32150ff57bb1d9c39
David Shanske <david@shanske.com>
2017-07-16 16:45:12 -0500

    Enhance Template

--- templates/linkbacks.php
+++ templates/linkbacks.php
@@ -1,51 +1,54 @@
-<?php if ( has_linkbacks( 'like' ) ) : ?>
-<div class="likes">
-	<h3>Likes</li>
-
 <?php 
+
+if ( has_linkbacks( 'like' ) ) {
+	echo '<div class="likes">';
+	echo '<h3>' . __( 'Likes', 'semantic-linkbacks' ) . '</h3>';
 	list_linkbacks( array(
 		'li-class' => array( 'single-mention', 'p-like' )
 	), 
 	get_linkbacks( 'like' ) ); ?>
-</div>
-<?php endif; ?>
+	echo '</div>';
+}
 
-<?php if ( has_linkbacks( 'repost' ) ) : ?>
-<h3>Reposts</li>
+if ( has_linkbacks( 'favorite' ) ) {
+	echo '<div class="favorite">';
+	echo '<h3>' . __( 'Favorites', 'semantic-linkbacks' ) . '</h3>';
+	list_linkbacks( array(
+		'li-class' => array( 'single-mention', 'p-favorite' )
+	), 
+	get_linkbacks( 'favorite' ) ); ?>
+	echo '</div>';
+}
 
-<ul>
-<?php foreach ( get_linkbacks( 'repost' ) as $repost ) : ?>
-	<li class="p-repost h-cite">
-		<span class="p-author h-card">
-			<a class="u-url p-name value-title" href="http://test2.de" title="<?php echo trim( strip_tags( get_comment_text() ) ); ?>">
-				<?php echo get_avatar( $repost, 50 ); ?>
-			</a>
-		</span>
-		<data class="u-url" value="http://test.de" />
-	</li>
-<?php endforeach; ?>
-</ul>
-<?php endif; ?>
+if ( has_linkbacks( 'bookmark' ) ) {
+	echo '<div class="bookmarks">';
+	echo '<h3>' . __( 'Bookmarks', 'semantic-linkbacks' ) . '</h3>';
+	list_linkbacks( array(
+		'li-class' => array( 'single-mention', 'p-bookmark' )
+	), 
+	get_linkbacks( 'bookmark' ) ); ?>
+	echo '</div>';
+}
 
-<?php if ( has_linkbacks( 'mention' ) ) : ?>
-<h3>Mention</li>
+if ( has_linkbacks( 'repost' ) ) {
+	echo '<div class="reposts">';
+	echo '<h3>' . __( 'Reposts', 'semantic-linkbacks' ) . '</h3>';
+	list_linkbacks( array(
+		'li-class' => array( 'single-mention', 'p-repost' )
+	), 
+	get_linkbacks( 'repost' ) ); ?>
+	echo '</div>';
+}
 
-<ul>
-<?php foreach ( get_linkbacks( 'mention' ) as $mention ) : ?>
-	<?php //var_dump( $mention ); ?>
-	<li><?php echo get_avatar( $mention, 50 ); ?></li>
-<?php endforeach; ?>
-</ul>
 
-<?php endif; ?>
+if ( has_linkbacks( 'mention' ) ) {
+	echo '<div class="mentions">';
+	echo '<h3>' . __( 'Mentions', 'semantic-linkbacks' ) . '</h3>';
+	list_linkbacks( array(
+		'li-class' => array( 'single-mention', 'p-comment' )
+	), 
+	get_linkbacks( 'mention' ) ); ?>
+	echo '</div>';
+}
 
-<?php if ( has_linkbacks( 'rsvp' ) ) : ?>
-<h3>RSVP</li>
 
-<ul>
-<?php foreach ( get_linkbacks( 'rsvp' ) as $rsvp ) : ?>
-	<li><?php echo get_avatar( $rsvp, 50 ); ?></li>
-<?php endforeach; ?>
-</ul>
-
-<?php endif; ?>
