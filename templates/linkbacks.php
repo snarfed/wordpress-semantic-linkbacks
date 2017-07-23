855a6706c5d0c8c88a78090112ed576063e1a363
David Shanske <david@shanske.com>
2017-07-22 22:58:46 -0500

    Add basic RSVP and tag support

--- templates/linkbacks.php
+++ templates/linkbacks.php
@@ -40,6 +40,63 @@ if ( has_linkbacks( 'repost' ) ) {
 	echo '</div>';
 }
 
+if ( has_linkbacks( 'tag' ) ) {
+	echo '<div class="tags">';
+	echo '<h3>' . __( 'Tags', 'semantic-linkbacks' ) . '</h3>';
+	list_linkbacks( array(
+		'li-class' => array( 'single-mention', 'p-tag' )
+	), 
+	get_linkbacks( 'tags' ) );
+	echo '</div>';
+}
+
+if ( has_linkbacks( 'rsvp:yes' ) || has_linkbacks( 'rsvp:no' ) || has_linkbacks( 'rsvp:maybe' ) || has_linkbacks( 'rsvp:invited' ) || has_linkbacks( 'rsvp:tracking' ) ) {
+	echo '<div class="rsvps">';
+	echo '<h3>' . __( 'RSVPs', 'semantic-linkbacks' ) . '</h3>';
+	if ( has_linkbacks( 'rsvp:yes' ) ) {
+		echo '<h4>' . __( 'Yes', 'semantic-linkbacks' ) . '</h4>';
+		list_linkbacks( array(
+			'li-class' => array( 'single-mention', 'p-rsvp' )
+		), 
+		get_linkbacks( 'rsvp:yes' ) );
+		echo '</div>';
+	}
+	if ( has_linkbacks( 'rsvp:no' ) ) {
+		echo '<h4>' . __( 'No', 'semantic-linkbacks' ) . '</h4>';
+		list_linkbacks( array(
+			'li-class' => array( 'single-mention', 'p-rsvp' )
+		), 
+		get_linkbacks( 'rsvp:no' ) );
+		echo '</div>';
+	}
+	if ( has_linkbacks( 'rsvp:maybe' ) ) {
+		echo '<h4>' . __( 'Maybe', 'semantic-linkbacks' ) . '</h4>';
+		list_linkbacks( array(
+			'li-class' => array( 'single-mention', 'p-rsvp' )
+		), 
+		get_linkbacks( 'rsvp:maybe' ) );
+		echo '</div>';
+	}
+	if ( has_linkbacks( 'rsvp:invited' ) ) {
+		echo '<h4>' . __( 'Invited', 'semantic-linkbacks' ) . '</h4>';
+		list_linkbacks( array(
+			'li-class' => array( 'single-mention', 'p-rsvp' )
+		), 
+		get_linkbacks( 'rsvp:invited' ) );
+		echo '</div>';
+	}
+	if ( has_linkbacks( 'rsvp:tracking' ) ) {
+		echo '<h4>' . __( 'Tracking', 'semantic-linkbacks' ) . '</h4>';
+		list_linkbacks( array(
+			'li-class' => array( 'single-mention', 'p-rsvp' )
+		), 
+		get_linkbacks( 'rsvp:tracking' ) );
+		echo '</div>';
+	}
+
+	echo '</div>';
+}
+
 
 if ( has_linkbacks( 'mention' ) ) {
 	echo '<div class="mentions">';
